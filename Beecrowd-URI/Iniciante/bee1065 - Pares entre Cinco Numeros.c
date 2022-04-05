0e5e1ef5cc567e7e43490737b88f550fce79bc51
Rafael Freitas <rafaarantes@live.com>
2022-04-04 21:39:54 -0300

    Update

---
+++ Beecrowd-URI/Iniciante/bee1065 - Pares entre Cinco Numeros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num, i, cont = 0;
+
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &num);
+        if(num % 2 == 0)
+            cont++;        
+    }
+
+    printf("%d valores pares\n", cont);
+    
+    return 0;
+}
