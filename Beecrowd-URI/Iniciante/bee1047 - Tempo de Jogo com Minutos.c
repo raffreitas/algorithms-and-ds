fc644e4b3b301b3600b261ab40178815da2def5a
Rafael <rafaarantes@live.com>
2022-05-23 20:15:33 -0300

    Update

---
+++ Beecrowd-URI/Iniciante/bee1047 - Tempo de Jogo com Minutos.c
@@ -0,0 +1,31 @@
+/*
+    Há um caso de teste que não funciona
+    Wrong answer (100%)
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+  int  hIni, hFim, mIni, mFim;
+  int horas, minutos;
+
+  scanf("%d %d %d %d", &hIni, &mIni, &hFim, &mFim);
+  
+  horas = hFim - hIni;
+  minutos = mFim - mIni;
+
+  if (horas <= 0)
+    horas += 24;
+
+  if (minutos < 0){
+    minutos += 60;
+    horas--;
+  }
+
+  printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", horas, minutos);
+
+  return 0;
+}
